8d5098c2ffe126a1d8f4c7a45d709bb5a52dbd0f
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-15 07:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const counterParagraph = document.getElementById("counter");
+const incrementButton = document.getElementById("incrementBtn");
+
+incrementButton.addEventListener('click', ()=>{
+	const currValue = parseInt(counterParagraph.textContent);
+	alert(`Current value: ${currValue}`);
+	counterParagraph.textContent = currValue+1;
+})
