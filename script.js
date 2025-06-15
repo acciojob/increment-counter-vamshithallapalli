4d609bbc2eaf4f02719e8b4336157df3d4a22a1b
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-15 07:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const incrementButton = document.getElementById("incrementBtn");
 
 incrementButton.addEventListener('click', ()=>{
 	const currValue = parseInt(counterParagraph.textContent);
+	const alertText = toString(currValue);
 	alert(`${currValue}`);
 	counterParagraph.textContent = currValue+1;
 })
